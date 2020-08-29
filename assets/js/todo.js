f296ee30c002d91b7db4b86a5cbd601dae939057
Upshur06 <mupshur2005@gmail.com>
2020-08-29 16:10:56 -0400

    toggle affect

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,4 @@
 //Check off specific todos by clicking
 $("li").on("click",function(){
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-    $(this).css({
-      color: "rgb(0, 0, 0)",
-      textDecoration: "none"
-    })
-  }else{
-    $(this).css({
-    color: "rgb(128, 128, 128)",
-    textDecoration: "line-through"
-    })
-  }
-})
+    $(this).toggleClass("turnGray");
+});
