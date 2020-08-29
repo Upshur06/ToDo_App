ac6fe5158517f00356989afad2917c86f1e788bf
Upshur06 <mupshur2005@gmail.com>
2020-08-29 15:58:02 -0400

    making logic

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,14 @@
-
+//Check off specific todos by clicking
+$("li").on("click",function(){
+  if($(this).css("color") === "rgb(128, 128, 128)"){
+    $(this).css({
+      color: "rgb(0, 0, 0)",
+      textDecoration: "none"
+    })
+  }else{
+    $(this).css({
+    color: "rgb(128, 128, 128)",
+    textDecoration: "line-through"
+    })
+  }
+})
