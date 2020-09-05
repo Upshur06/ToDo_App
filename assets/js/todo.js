19147ffefd88c8e637686349b7b32d2904c3c6d9
Upshur06 <mupshur2005@gmail.com>
2020-09-05 14:52:10 -0400

    remove list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,14 @@
-//Check off specific todos by clicking
+// Check off specific todos by clicking
 $("li").on("click",function(){
     $(this).toggleClass("turnGray");
+});
+
+// alert("Hello from app.js");
+
+// Click on "x" to delete Todo
+$("span").on("click",function(event){
+  $(this).parent().fadeOut(2000,function(){
+    $(this).remove();
+  });
+  event.stopPropgation();
 });
