9a6ce9b083256f39e83928ab276c8bd2532b8e2e
Upshur06 <mupshur2005@gmail.com>
2020-09-26 12:14:45 -0400

    append list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,21 @@
 // Check off specific todos by clicking
-$("li").on("click",function(){
+$("ul").on("click","li", function(){
     $(this).toggleClass("turnGray");
 });
 
 // alert("Hello from app.js");
 
 // Click on "x" to delete Todo
-$("span").on("click",function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(2000,function(){
-    $(this).remove();
   });
   event.stopPropgation();
 });
+
+$("input[type=text]").on("keypress",function(event){
+  if(event.which === 13){
+    let todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+  }
+})
