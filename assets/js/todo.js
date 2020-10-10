7d57bea108a4994fba96c272ff34b3e6cafed8d6
Upshur06 <mupshur2005@gmail.com>
2020-10-10 17:04:04 -0400

    working out some bugs

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -12,10 +12,23 @@ $("ul").on("click", "span", function(event){
   event.stopPropgation();
 });
 
+// press enter and the list and text will append to the li in list-container
 $("input[type=text]").on("keypress",function(event){
   if(event.which === 13){
     let todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+    $("ul").append("<li>" + "<span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
   }
 })
+
+$(".fa-plus").on("click",function(){
+  $("input[type=text]").fadeToggle();
+  $("input[type=date]").fadeToggle();
+});
+
+
+
+// list to be append based on Date
+// list display time as well
+// list to be sectioned based on specific day
+// if list the list isn't complete by the specific day or time it goes into the pass due section
